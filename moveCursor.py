2bb49b65b46a4f78bbe9904ca8e401d5e8554f0a
Ayush Gupta <gupta07ayush@gmail.com>
2023-03-25 16:44:13 +0530

    Update moveCursor.py
    edited the sleep time

--- moveCursor.py
+++ moveCursor.py
@@ -1,11 +1,10 @@
 #This python program will move your mouse cursor, 1cm down per second from starting point
-#NOTE: to stop the program move your cursor to the top left corner of your screen.
 
 import pyautogui
 import time
 while True:
 	for i in range(0,100):
 		pyautogui.moveTo(1000,i*10)
-		time.sleep(1)
+		time.sleep(0)
   
   
